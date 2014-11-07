3ee08589c5475ad61935ae9000162a84e239c418
Nathan Malcolm <nmalcolm@users.noreply.github.com>
2014-11-07 04:56:43 +0000

    Add basic navigation and index page

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,6 +7,31 @@
     {{ stylesheet_link_tag() }}
   </head>
   <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Prometheus</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Explore</a></li>
+            <li><a href="#">Developers</a></li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Signup</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
     @yield('content')
 
     {{ javascript_include_tag() }}
